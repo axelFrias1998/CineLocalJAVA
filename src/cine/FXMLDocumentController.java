5e20193a0f44a92c9be1cc5f1e5f0c19df8f5f18
Axel Frías Hernández <axel.frias257@gmail.com>
2019-05-14 01:52:15 -0500

    Login con diseño

--- src/cine/FXMLDocumentController.java
+++ src/cine/FXMLDocumentController.java
@@ -27,20 +27,7 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/CineDB","root","");
-            //here sonoo is the database name, root is the username and root is the password
-            Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select * from CineDB.TipoProyeccion");
-            
-            while(rs.next())
-            System.out.println(rs.getInt(1) + " " + rs.getString(2) + " $" + rs.getFloat(3));
-
-            con.close();
-
-        }catch(Exception e){ System.out.println(e);}
+        
     }
     
     @Override
--- src/cine/LoginController.java
+++ src/cine/LoginController.java
@@ -5,12 +5,26 @@
  */
 package cine;
 
+
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDecorator;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -22,26 +36,61 @@ import javafx.stage.Stage;
  */
 public class LoginController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private JFXButton btnInicia;
+
+    @FXML
+    private JFXTextField txtCorreo;
+
+    @FXML
+    private JFXPasswordField txtContrasenia;
+
+    @FXML
+    void IniciaSesion(ActionEvent event) throws IOException, ClassNotFoundException {
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CineDB","root","");
+            
+            CallableStatement call = con.prepareCall("{call inicioSesion_sp(?)}");
+            call.setString(1, txtCorreo.getText());
+            
+            boolean hash = call.execute();
+            
+            if(hash){
+                  try(ResultSet rs = call.getResultSet()){
+                      if(rs.next()){
+                          String spPass = rs.getString(1);
+                          System.out.println(spPass);
+                          System.out.println(txtContrasenia.getText());
+                          if(spPass.equalsIgnoreCase(txtContrasenia.getText()))
+                              System.out.println("EXISTE Y JAJSDAJS");
+                          else
+                              System.out.println("Vale verga");
+                      }
+                  }catch(SQLException ex){}
+//                Stage actual = (Stage)((Node)event.getSource()).getScene().getWindow();
+//                actual.hide();
+//
+//                Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+//                Stage nuevo = new Stage();
+//                JFXDecorator decorator = new JFXDecorator(nuevo, root); 
+//                Scene scene = new Scene(decorator);
+//                nuevo.setScene(scene);
+//                nuevo.show();
+            }
+            else
+                System.out.println("Sin respuesta");
+
+       
+            con.close();
+
+        }catch(SQLException e){ System.out.println(e);}
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
-    }    
-    
-    public void IniciaSesion() throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
-        Stage stage = new Stage();
-        
-        JFXDecorator decorator = new JFXDecorator(stage, root); 
-        Scene scene = new Scene(decorator);
-        
-        stage.setScene(scene);
-        stage.show();
-        
-        Stage window = null;
-        window.setScene(scene);
-    }
-    
+    }        
 }
